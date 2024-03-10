3e419448ef05694aff5b23d2bb697175bbd4e3e5
kimquynh <quynh.nguyenkim1304@hcmut.edu.vn>
2024-03-10 23:58:59 +0700

    add Lab3

--- Lab03/main.py
+++ Lab03/main.py
@@ -3,6 +3,8 @@ import time
 import random
 from Adafruit_IO import MQTTClient
 import random
+from uart import *
+from simple_ai import *
 
 AIO_FEED_ID = ["nutnhan1","nutnhan2"]
 AIO_USERNAME = "kimquynh1304"
@@ -22,6 +24,16 @@ def disconnected(client):
 
 def message(client , feed_id , payload):
     print("Nhan du lieu: " + payload + " , feed id: " + feed_id)
+    if feed_id == "nutnhan1":
+        if payload == "0":
+            writeSerial("1")
+        else:    
+            writeSerial("2")
+    elif feed_id == "nutnhan2":
+        if payload == "0":
+            writeSerial("3")
+        else:    
+            writeSerial("4")
 
 client = MQTTClient(AIO_USERNAME , AIO_KEY)
 client.on_connect = connected
@@ -33,6 +45,9 @@ client.loop_background()
 sensor_type = 0
 
 counter = 10
+counter_ai = 5
+previous_result = ""
+ai_result = ""
 while True:
     counter = counter - 1
     if counter <= 0:
@@ -54,5 +69,15 @@ while True:
             humi = random.randint(0,100)
             client.publish("cambien3",humi)
             sensor_type = 0 
+    
+    # counter_ai = counter_ai - 1
+    # if counter_ai <= 0:
+    #     counter_ai = 5
+    #     previous_result = ai_result
+    #     ai_result = image_detector()
+    #     print("AI output",ai_result)
+    #     if previous_result != ai_result:
+    #         client.publish("ai",ai_result)
+    readSerial(client)
     time.sleep(1)
     pass
